d154eb763eec0dc272c8dfa9a1d135be083da53b
ThanhTuyen-304 <tuyenc681@gmail.com>
2026-02-13 14:46:12 +0700

    ....

---
+++ src/KhuVucLuuTru_DTO.java
@@ -0,0 +1,59 @@
+
+public class KhuVucLuuTru_DTO {
+    private String maKVLT;
+    private String tenKVLT;
+    private String ngayLapKho;
+    private int sucChua;
+    private int trangThai;
+    private String maDC;
+
+    public KhuVucLuuTru_DTO(String maKVLT, String tenKVLT, String ngayLapKho, int sucChua, int trangThai, String maDC) {
+        this.maKVLT = maKVLT;
+        this.tenKVLT = tenKVLT;
+        this.ngayLapKho = ngayLapKho;
+        this.sucChua = sucChua;
+        this.trangThai = trangThai;
+        this.maDC = maDC;
+    }
+
+    public String getMaKVLT() {
+        return maKVLT;
+    }
+    public void setMaKVLT(String maKVLT) {
+        this.maKVLT = maKVLT;
+    }
+
+    public String getTenKVLT() {
+        return tenKVLT;
+    }
+    public void setTenKVLT(String tenKVLT) {
+        this.tenKVLT = tenKVLT;
+    }
+
+    public String getNgayLapKho() {
+        return ngayLapKho;
+    }
+    public void setNgayLapKho(String ngayLapKho) {
+        this.ngayLapKho = ngayLapKho;
+    }
+
+    public int getSucChua() {
+        return sucChua; }
+    public void setSucChua(int sucChua) {
+        this.sucChua = sucChua;
+    }
+
+    public int getTrangThai() {
+        return trangThai;
+    }
+    public void setTrangThai(int trangThai) {
+        this.trangThai = trangThai;
+    }
+
+    public String getMaDC() {
+        return maDC;
+    }
+    public void setMaDC(String maDC) {
+        this.maDC = maDC;
+    }
+}
---
+++ src/NhaCungCap_DTO.java
@@ -0,0 +1,69 @@
+
+public class NhaCungCap_DTO {
+    private String maNCC;
+    private String tenNCC;
+    private String nguoiLienHe;
+    private String sdt;
+    private String maSoThue;
+    private int trangThai;
+    private String maDC;
+
+
+    public NhaCungCap_DTO(String maNCC, String tenNCC, String nguoiLienHe, String sdt, String maSoThue, int trangThai, String maDC) {
+        this.maNCC = maNCC;
+        this.tenNCC = tenNCC;
+        this.nguoiLienHe = nguoiLienHe;
+        this.sdt = sdt;
+        this.maSoThue = maSoThue;
+        this.trangThai = trangThai;
+        this.maDC = maDC;
+    }
+
+    public String getMaNCC() {
+        return maNCC;
+    }
+    public void setMaNCC(String maNCC) {
+        this.maNCC = maNCC;
+    }
+
+    public String getTenNCC() {
+        return tenNCC;
+    }
+    public void setTenNCC(String tenNCC) {
+        this.tenNCC = tenNCC;
+    }
+
+    public String getNguoiLienHe() {
+        return nguoiLienHe;
+    }
+    public void setNguoiLienHe(String nguoiLienHe) {
+        this.nguoiLienHe = nguoiLienHe;
+    }
+
+    public String getSdt() {
+        return sdt;
+    }
+    public void setSdt(String sdt) {
+        this.sdt = sdt;
+    }
+
+    public String getMaSoThue() {
+        return maSoThue;
+    }
+    public void setMaSoThue(String maSoThue) {
+        this.maSoThue = maSoThue;
+    }
+
+    public int getTrangThai() {
+        return trangThai;
+    }
+    public void setTrangThai(int trangThai) {
+        this.trangThai = trangThai;
+    }
+    public String getMaDC() {
+        return maDC;
+    }
+    public void setMaDC(String maDC) {
+        this.maDC = maDC;
+    }
+}
