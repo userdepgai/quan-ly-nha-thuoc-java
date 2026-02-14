45f1242d9c28d3601b72792abfdacd5630254adf
ThanhTuyen-304 <tuyenc681@gmail.com>
2026-02-14 22:27:14 +0700

    ....

--- src/dto/SanPhamNCC_DTO.java
+++ src/dto/SanPhamNCC_DTO.java
@@ -1,35 +1,42 @@
 package dto;
 
 public class SanPhamNCC_DTO {
-    private String maSP;
-    private String maNCC;
+
+    private String sanPham;
+    private String nhaCungCap;
     private double giaNhap;
     private int trangThai;
 
-    public SanPhamNCC_DTO(String maSP, String maNCC, double giaNhap, int trangThai) {
-        this.maSP = maSP;
-        this.maNCC = maNCC;
+
+    public SanPhamNCC_DTO(String sanPham, String nhaCungCap, double giaNhap, int trangThai) {
+        this.sanPham = sanPham;
+        this.nhaCungCap = nhaCungCap;
         this.giaNhap = giaNhap;
         this.trangThai = trangThai;
     }
 
-    public String getMaSP() {
-        return maSP;
-    }
-    public void setMaSP(String maSP) {
-        this.maSP = maSP;
+
+    public String getSanPham() {
+        return sanPham;
     }
 
-    public String getMaNCC() {
-        return maNCC;
+    public void setSanPham(String sanPham) {
+        this.sanPham = sanPham;
     }
-    public void setMaNCC(String maNCC) {
-        this.maNCC = maNCC;
+
+
+    public String getNhaCungCap() {
+        return nhaCungCap;
+    }
+
+    public void setNhaCungCap(String nhaCungCap) {
+        this.nhaCungCap = nhaCungCap;
     }
 
     public double getGiaNhap() {
         return giaNhap;
     }
+
     public void setGiaNhap(double giaNhap) {
         this.giaNhap = giaNhap;
     }
@@ -37,6 +44,7 @@ public class SanPhamNCC_DTO {
     public int getTrangThai() {
         return trangThai;
     }
+
     public void setTrangThai(int trangThai) {
         this.trangThai = trangThai;
     }
